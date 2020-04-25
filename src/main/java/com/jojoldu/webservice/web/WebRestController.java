ef674a68cd02feed39cb04c99e1c0d444c8a84e0
lowoon <sangbo0229@gmail.com>
2020-04-25 20:12:33 +0900

    feat: save createdDate and modifiedDate

--- src/main/java/com/jojoldu/webservice/web/WebRestController.java
+++ src/main/java/com/jojoldu/webservice/web/WebRestController.java
@@ -1,13 +1,27 @@
 package com.jojoldu.webservice.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jojoldu.webservice.domain.posts.PostsRepository;
+import com.jojoldu.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
--- src/test/java/com/jojoldu/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/webservice/domain/posts/PostsRepositoryTest.java
@@ -2,7 +2,10 @@ package com.jojoldu.webservice.domain.posts;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import javax.validation.constraints.AssertTrue;
 
 import org.junit.After;
 import org.junit.Test;
@@ -39,5 +42,24 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo("테스트 게시글");
         assertThat(posts.getContent()).isEqualTo("테스트 본문");
+    }
+
+    @Test
+    public void saveBaseTimeEntity() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("jojoldu@gmail.com")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
